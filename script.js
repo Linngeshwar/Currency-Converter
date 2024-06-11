8f9047ca77d1a7dee0614fc824f603e950ae2670
Linngeshwar <158817859+Linngeshwar@users.noreply.github.com>
2024-06-11 15:50:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,233 @@
+const country_list = {
+    "AED" : "AE",
+    "AFN" : "AF",
+    "XCD" : "AG",
+    "ALL" : "AL",
+    "AMD" : "AM",
+    "ANG" : "AN",
+    "AOA" : "AO",
+    "AQD" : "AQ",
+    "ARS" : "AR",
+    "AUD" : "AU",
+    "AZN" : "AZ",
+    "BAM" : "BA",
+    "BBD" : "BB",
+    "BDT" : "BD",
+    "XOF" : "BE",
+    "BGN" : "BG",
+    "BHD" : "BH",
+    "BIF" : "BI",
+    "BMD" : "BM",
+    "BND" : "BN",
+    "BOB" : "BO",
+    "BRL" : "BR",
+    "BSD" : "BS",
+    "NOK" : "BV",
+    "BWP" : "BW",
+    "BYR" : "BY",
+    "BZD" : "BZ",
+    "CAD" : "CA",
+    "CDF" : "CD",
+    "XAF" : "CF",
+    "CHF" : "CH",
+    "CLP" : "CL",
+    "CNY" : "CN",
+    "COP" : "CO",
+    "CRC" : "CR",
+    "CUP" : "CU",
+    "CVE" : "CV",
+    "CYP" : "CY",
+    "CZK" : "CZ",
+    "DJF" : "DJ",
+    "DKK" : "DK",
+    "DOP" : "DO",
+    "DZD" : "DZ",
+    "ECS" : "EC",
+    "EEK" : "EE",
+    "EGP" : "EG",
+    "ETB" : "ET",
+    "EUR" : "FR",
+    "FJD" : "FJ",
+    "FKP" : "FK",
+    "GBP" : "GB",
+    "GEL" : "GE",
+    "GGP" : "GG",
+    "GHS" : "GH",
+    "GIP" : "GI",
+    "GMD" : "GM",
+    "GNF" : "GN",
+    "GTQ" : "GT",
+    "GYD" : "GY",
+    "HKD" : "HK",
+    "HNL" : "HN",
+    "HRK" : "HR",
+    "HTG" : "HT",
+    "HUF" : "HU",
+    "IDR" : "ID",
+    "ILS" : "IL",
+    "INR" : "IN",
+    "IQD" : "IQ",
+    "IRR" : "IR",
+    "ISK" : "IS",
+    "JMD" : "JM",
+    "JOD" : "JO",
+    "JPY" : "JP",
+    "KES" : "KE",
+    "KGS" : "KG",
+    "KHR" : "KH",
+    "KMF" : "KM",
+    "KPW" : "KP",
+    "KRW" : "KR",
+    "KWD" : "KW",
+    "KYD" : "KY",
+    "KZT" : "KZ",
+    "LAK" : "LA",
+    "LBP" : "LB",
+    "LKR" : "LK",
+    "LRD" : "LR",
+    "LSL" : "LS",
+    "LTL" : "LT",
+    "LVL" : "LV",
+    "LYD" : "LY",
+    "MAD" : "MA",
+    "MDL" : "MD",
+    "MGA" : "MG",
+    "MKD" : "MK",
+    "MMK" : "MM",
+    "MNT" : "MN",
+    "MOP" : "MO",
+    "MRO" : "MR",
+    "MTL" : "MT",
+    "MUR" : "MU",
+    "MVR" : "MV",
+    "MWK" : "MW",
+    "MXN" : "MX",
+    "MYR" : "MY",
+    "MZN" : "MZ",
+    "NAD" : "NA",
+    "XPF" : "NC",
+    "NGN" : "NG",
+    "NIO" : "NI",
+    "NPR" : "NP",
+    "NZD" : "NZ",
+    "OMR" : "OM",
+    "PAB" : "PA",
+    "PEN" : "PE",
+    "PGK" : "PG",
+    "PHP" : "PH",
+    "PKR" : "PK",
+    "PLN" : "PL",
+    "PYG" : "PY",
+    "QAR" : "QA",
+    "RON" : "RO",
+    "RSD" : "RS",
+    "RUB" : "RU",
+    "RWF" : "RW",
+    "SAR" : "SA",
+    "SBD" : "SB",
+    "SCR" : "SC",
+    "SDG" : "SD",
+    "SEK" : "SE",
+    "SGD" : "SG",
+    "SKK" : "SK",
+    "SLL" : "SL",
+    "SOS" : "SO",
+    "SRD" : "SR",
+    "STD" : "ST",
+    "SVC" : "SV",
+    "SYP" : "SY",
+    "SZL" : "SZ",
+    "THB" : "TH",
+    "TJS" : "TJ",
+    "TMT" : "TM",
+    "TND" : "TN",
+    "TOP" : "TO",
+    "TRY" : "TR",
+    "TTD" : "TT",
+    "TWD" : "TW",
+    "TZS" : "TZ",
+    "UAH" : "UA",
+    "UGX" : "UG",
+    "USD" : "US",
+    "UYU" : "UY",
+    "UZS" : "UZ",
+    "VEF" : "VE",
+    "VND" : "VN",
+    "VUV" : "VU",
+    "YER" : "YE",
+    "ZAR" : "ZA",
+    "ZMK" : "ZM",
+    "ZWD" : "ZW"
+}
+const dropList = document.querySelectorAll("select");
+const exchangeCurrencies = document.getElementById("exchange");
+const fromCurrency = document.querySelector(".from select");
+const toCurrency = document.querySelector(".to select");
+const exchangeButton = document.getElementById("getExchangeRate");
+const selections = document.getElementById("selections");
+const conatiner = document.getElementById("container");
+
+for(let i = 0 ; i < dropList.length ; i++){
+    for(let currencyCode in country_list){
+        let selected;
+        selected = i==0 ? 
+        currencyCode == "USD" ? "selected" : "" 
+        : currencyCode == "INR" ? "selected" : "";
+        let optionTag = `<option value="${currencyCode}" ${selected}>${currencyCode}</option>`;
+        dropList[i].insertAdjacentHTML("beforeend",optionTag)
+    }
+    dropList[i].addEventListener("change", (e) =>{
+        loadFlag(e.target)
+    });
+}
+function loadFlag(element){
+    for(let code in country_list){
+        if(code==element.value){
+            let imgTag = element.parentElement.querySelector("img");
+            imgTag.src = `https://flagcdn.com/48x36/${country_list[code].toLowerCase()}.png`
+        }
+    }
+}
+exchangeCurrencies.addEventListener("click", () => {
+    let temp = fromCurrency.value;
+    fromCurrency.value = toCurrency.value;
+    toCurrency.value = temp;
+    loadFlag(fromCurrency);
+    loadFlag(toCurrency);
+});
+
+function getExchangeRate(){
+    const amount = document.getElementById("amount");
+    const result = document.getElementById("answer");
+    let amountVal = amount.value;
+    if(amountVal == ""){
+        amount.value = "1";
+        amountVal=1;
+    }
+    if(!Number(amountVal)){
+        result.classList.add("alert");
+        result.innerText = "Invalid Amount";
+        return;
+    }
+    result.classList.remove("alert");
+    let url = `https://v6.exchangerate-api.com/v6/51eb527b42e7065a37971301/latest/${fromCurrency.value}`;
+    fetch(url).then(
+        response => response.json())
+        .then(
+            result => {
+                let exchangeRate = result.conversion_rates[toCurrency.value];
+                let answer = (amountVal * exchangeRate).toFixed(2);
+                let final = `${amountVal} = ${answer}`
+                console.log(final);
+                document.getElementById("answer").innerText = final;
+            }
+        );
+}
+exchangeButton.addEventListener("click",()=>{
+    getExchangeRate();
+})
+
+window.addEventListener("load",(e)=>{
+    e.preventDefault();
+    getExchangeRate();
+})
